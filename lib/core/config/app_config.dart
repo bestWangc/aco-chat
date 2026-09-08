618b00c2029545a661545beb597acb0464173286
Top <wangc_web3@163.com>
2026-09-08 15:40:26 +0800

    升级应用版本并准备安卓和 iOS 包

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const appVersion = String.fromEnvironment(
     'ACO_APP_VERSION',
-    defaultValue: '1.0.38',
+    defaultValue: '1.0.39',
   );
 
   static const cloudflareApiBaseUrl = String.fromEnvironment(
